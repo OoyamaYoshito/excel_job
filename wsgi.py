3c38703a30ee2ef88c32907a10f0e8fb454c34cb
伊藤恵研究室 <member@keilab-macmini2018.local>
2020-10-07 10:20:46 +0900

    学籍番号指定時の処理

--- wsgi.py
+++ wsgi.py
@@ -21,11 +21,24 @@ def output_excel(stgrade, stclass, outpath):
             zip.write(outpath + '/' + f, arcname=f)
     return zipname
 
+def output_excel2(students, outpath):
+    import output2
+    student="/Users/member/git/excel_job/studentlist"
+    answer="/Users/member/git/excel_job/answersdata"
+    import os
+    os.chdir(os.path.dirname(__file__))
+    try:
+        output2.output_students(students,outputpath=outpath,studentpath=student,answerpath=answer)
+    except ValueError as e:
+        return e.args[0]
+    return outpath + '/output.xlsx'
+
 def wsgi_app(environ, start_response):
     import sys
     import pprint
     import os
     import re
+    import urllib
     sys.path.append('/Users/member/git/excel_job')
     import output
     #output = pprint.pformat(os.listdir('.')) + '\n'
@@ -33,6 +46,7 @@ def wsgi_app(environ, start_response):
     select = ''
     stgrade = ''
     stclass = ''
+    students = ''
     for q in environ['QUERY_STRING'].split('&'):
         if q.startswith('select='):
             select=q.replace('select=','')
@@ -40,6 +54,8 @@ def wsgi_app(environ, start_response):
             stgrade=q.replace('grade=','')
         if q.startswith('class='):
             stclass+=q.replace('class=','')
+        if q.startswith('students='):
+            students+=q.replace('students=','')
     if select == 'byclass' and stgrade != '' and stclass != '':
         import tempfile
         outpath=tempfile.mkdtemp()
@@ -56,11 +72,45 @@ def wsgi_app(environ, start_response):
             output = output.encode('utf-8')
             headers = [('Content-type', 'text/html; charset=utf-8'),
                        ('Content-Length', str(len(output)))]
+    elif select == 'bystudents' and students != '':
+        output = urllib.parse.unquote(students)
+        students = output.split()
+        import tempfile
+        outpath=tempfile.mkdtemp()
+        zip = output_excel2(students,outpath)
+        if os.path.isfile(zip):
+            headers = [('Content-type', 'application/excel'),
+                       ('Content-Length', str(os.path.getsize(zip))),
+                       ('Content-Disposition', 'attachment; filename="' + os.path.basename(zip) + '"')]
+            with open(zip,'rb') as f:
+                output=f.read()
+                f.close()
+        else:
+            output = zip
+            output = output.encode('utf-8')
+            headers = [('Content-type', 'text/html; charset=utf-8'),
+                       ('Content-Length', str(len(output)))]
     else:
-        output = '<form>'
+        output = '<script type="text/javascript">'
+        output += 'function byclass() {'
+        output += '  var x = document.getElementsByName("grade");'
+        output += '  for (i = 0; i < x.length; i++) x[i].disabled = false;'
+        output += '  var x = document.getElementsByName("class");'
+        output += '  for (i = 0; i < x.length; i++) x[i].disabled = false;'
+        output += '  document.getElementsByName("students")[0].disabled = true;'
+        output += '}'
+        output += 'function bystudents() {'
+        output += '  var x = document.getElementsByName("grade");'
+        output += '  for (i = 0; i < x.length; i++) x[i].disabled = true;'
+        output += '  var x = document.getElementsByName("class");'
+        output += '  for (i = 0; i < x.length; i++) x[i].disabled = true;'
+        output += '  document.getElementsByName("students")[0].disabled = false;'
+        output += '}'
+        output += '</script>'
+        output += '<form>'
         output += 'Select: '
-        output += '<input type="radio" name="select" value="byclass" checked>By Class'
-        output += '<input type="radio" name="select" value="bystudents">By Students'
+        output += '<input type="radio" name="select" value="byclass" checked onclick="byclass()">By Class'
+        output += '<input type="radio" name="select" value="bystudents" onclick="bystudents()">By Student IDs'
         output += '<br/>'
         output += 'Grade: '
         for g in [1,2]:
