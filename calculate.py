61e785bc36076ceb1e3bc7937a70c06c92836ad1
伊藤恵研究室 <member@keilab-macmini2018.local>
2021-09-30 09:20:48 +0900

    StudentListの更新に追従

--- calculate.py
+++ calculate.py
@@ -167,6 +167,7 @@ def get_classstudents(age, class_name, studentpath="studentlist"):
         df_classdata = df_studentlist.query("(Grade == @age)&(Class == @class_name)")
     df_classdata = df_classdata.drop(columns=["Absence","Ent.year"])
     df_classdata = df_classdata.drop(columns=["Name(J_Kana)","Name(E) "],errors='ignore')
+    df_classdata = df_classdata.drop(columns=["Name(J Kana)","Name(E)"],errors='ignore')
     df_classdata = df_classdata.drop(columns=["Name\n(J Kana)","Name(E)"],errors='ignore')
     df_classdata = df_classdata.drop(columns=["Name_J kana","Name_E"],errors='ignore')
     df_classdata = df_classdata.iloc[:,[0,1,3,4,2]]
